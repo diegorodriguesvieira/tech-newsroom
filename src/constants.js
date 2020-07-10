3bca75ef867dd3e9dd85d78c2160ee33c015e522
Diego Rodrigues Vieira <diegorodrigues92@gmail.com>
2020-07-10 12:22:29 -0300

    remove magical numbers

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
+export const httpDelays = {
+  normal: 300,
+};
+
 export const filters = {
   asc: "asc",
   desc: "desc",
 };
-
-export default filters;
--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,11 @@
 import pMinDelay from "p-min-delay";
+import { httpDelays } from "../constants";
 import api from "./api";
 
 export function fetchPosts() {
-  return pMinDelay(api.get("/5be5e3fa2f000082000fc3f8"), 300);
+  return pMinDelay(api.get("/5be5e3fa2f000082000fc3f8"), httpDelays.normal);
 }
 
 export function fetchAuthors() {
-  return pMinDelay(api.get("/5be5e3ae2f00005b000fc3f6"), 300);
+  return pMinDelay(api.get("/5be5e3ae2f00005b000fc3f6"), httpDelays.normal);
 }
